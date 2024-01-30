d4ddbba9c87f1967731516ba937f81dfaa0cc51b
ashishmohanty10 <ashishmohaty10@gmail.com>
2024-01-30 21:05:31 +0530

    added assests

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import BgImg from "./assets/desktop.svg";
+import MobileBG from "./assets/mobile.svg";
+import Star from "./assets/icon-star.svg";
+import Plus from "./assets/icon-plus.svg";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <img src={BgImg} alt="" className="w-full" />
 
-export default App
+        {/* Main Div */}
+
+        <div className="flex justify-center items-center m-auto ">
+          {/* heading */}
+
+          <div className="bg-white w-[500px] h-[650px] absolute border border-slate-400 p-4">
+            <div className="flex items-center justify-start gap-4">
+              <img src={Star} alt="" />
+              <h1 className="text-xl font-bold">FAQS</h1>
+            </div>
+
+            {/* Accordion Div */}
+
+            <div>
+              <div className="flex justify-between items-start">
+                <div>
+                  <h3>Questions:</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Vero voluptate quam ipsam ex assumenda quae voluptatibus,
+                    dicta quia reiciendis. Possimus soluta quae laborum facilis
+                    hic voluptatum repellat optio veritatis expedita!
+                  </p>
+                </div>
+
+                <img src={Plus} alt="" />
+              </div>
+
+              <div className="flex justify-between items-center">
+                <div>
+                  <h3>Questions:</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Vero voluptate quam ipsam ex assumenda quae voluptatibus,
+                    dicta quia reiciendis. Possimus soluta quae laborum facilis
+                    hic voluptatum repellat optio veritatis expedita!
+                  </p>
+                </div>
+
+                <img src={Plus} alt="" />
+              </div>
+
+              <div className="flex justify-between items-center">
+                <div>
+                  <h3>Questions:</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Vero voluptate quam ipsam ex assumenda quae voluptatibus,
+                    dicta quia reiciendis. Possimus soluta quae laborum facilis
+                    hic voluptatum repellat optio veritatis expedita!
+                  </p>
+                </div>
+
+                <img src={Plus} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
